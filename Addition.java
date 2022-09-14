c5f750b15fdb8c2d93fe6f2874ac34cf268939d7
Dharshiniaathi <113535194+Dharshiniaathi@users.noreply.github.com>
2022-09-14 21:28:11 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,13 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int n1, n2, Result;
+		n1=24;
+		n2=12;
+		Result=n1+n2;
+		System.out.println("Result= "+Result);
+		System.out.println("The Addition of "+n1+" and "+n2+"is "+Result);
+	}
+
+}

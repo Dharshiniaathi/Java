092aea77ff5db48d86599cbf2f86bf8150e9c673
Dharshiniaathi <113535194+Dharshiniaathi@users.noreply.github.com>
2022-09-14 21:29:38 +0530

    Add files via upload

---
+++ Subtraction.java
@@ -0,0 +1,13 @@
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+		int n1, n2, Result;
+		n1=23;
+		n2=13;
+		Result=n1-n2;
+		System.out.println("Result= "+Result);
+		System.out.println("The Subtraction of "+n1+" and "+n2+"is "+Result);
+	}
+
+}

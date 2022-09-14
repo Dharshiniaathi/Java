7acac159fb2be564b5868fbc47d108a577c6fe9c
Dharshiniaathi <113535194+Dharshiniaathi@users.noreply.github.com>
2022-09-14 21:29:03 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		int n1, n2, Result;
+		n1=15;
+		n2=3;
+		Result=n1*n2;
+		System.out.println("Result "+Result);
+		System.out.println("The Multiplication of "+n1+" and "+n2+" is "+Result);
+	}
+
+}

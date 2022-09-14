34eb0e3f97ef829806acfa79e51fb30f8bac2ebd
Dharshiniaathi <113535194+Dharshiniaathi@users.noreply.github.com>
2022-09-14 20:20:51 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,26 @@
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		int i=10;
+		byte b=126;
+		short s=567;
+		long l=876L;
+		float f=567.5f;
+		double d=87998.56;
+		char ch='B';
+		char chr=66;// 65 to 99 (A-Z) 97-122 (a-z)
+		boolean bool=true;
+		System.out.println("Integer Value = "+i);
+		System.out.println("Byte Value = "+b);
+		System.out.println("Short Value = "+s);
+		System.out.println("Long Value = "+l);
+		System.out.println("Float Value = "+f);
+		System.out.println("Double Value = "+d);
+		System.out.println("Char Value = "+ch);
+		System.out.println("Character Value = "+chr);
+		System.out.println("Boolean Value = "+bool);
+
+	}
+
+}

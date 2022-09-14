4eae1a121e470be3891f3d85a48926765d408599
Dharshiniaathi <113535194+Dharshiniaathi@users.noreply.github.com>
2022-09-14 21:28:39 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,14 @@
+
+public class Division {
+
+	public static void main(String[] args) {
+		int n1, n2, Result;
+		n1=1000;
+		n2=10;
+		Result=n1/n2;
+		System.out.println("Result= "+Result);
+		System.out.println("The Addition of "+n1+" and "+n2+"is "+Result);
+
+	}
+
+}

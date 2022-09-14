86cfdb54d833eec1ffab7897a10064cf85b37222
Dharshiniaathi <113535194+Dharshiniaathi@users.noreply.github.com>
2022-09-14 21:31:35 +0530

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,12 @@
+
+public class Triangle {
+
+	public static void main(String[] args) {
+		int b, h, Area;
+		b=8;
+		h=12;
+		Area=(b*h)/2;
+		System.out.println("Area of Triangle = "+Area);
+	}
+
+}
